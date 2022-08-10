1e1ef78098a7d1db4ff5c51c4288f332c0e8c007
Facu00H <faisher00@gmail.com>
2022-08-10 14:41:31 -0300

    componentes menu desplegable + carrito

---
+++ scripts/components.js
@@ -0,0 +1,18 @@
+const menu = document.getElementById('burguer');
+const nav = document.getElementById('nav');
+const carrito = document.getElementById('carrito');
+const listaCarrito = document.getElementById('listaCarrito');
+console.log(listaCarrito)
+
+let contador = 0
+menu.addEventListener('click', () =>{
+  contador ++
+  contador % 2 != 0 ? nav.classList.remove('hidden2') : nav.classList.add('hidden2')
+})
+
+
+let contador2 = 0
+carrito.addEventListener('click', () =>{
+  contador2 ++
+  contador2 % 2 != 0 ? listaCarrito.classList.remove('hidden1') : listaCarrito.classList.add('hidden1')
+})
