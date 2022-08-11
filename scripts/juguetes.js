ea8c739195becce7cce1deabfa4a4629567b5244
juanPedroGalarza <juanpg232@gmail.com>
2022-08-11 10:36:09 -0300

    responsive cards juguetes

--- scripts/juguetes.js
+++ scripts/juguetes.js
@@ -46,24 +46,20 @@ function printCards(arrayData) {
     cardsContainer.innerHTML= ""
     arrayData.forEach(producto => {
     let newCard = document.createElement("div")
-    newCard.className = "cards border border-1  p-3 m-4  shadow justify-content-center"
-    newCard.style.width = "20rem"
-    newCard.style.height = "30rem"
+    newCard.className = "border border-1 p-3 shadow justify-content-center card-articulo"
     newCard.innerHTML=`
-        <div class="card shadow" style="width: 100%; height: 28rem;">
+        <div class="card shadow">
             
-            <img src="${producto.imagen}" style="height: 250px; width: 100%" >
-               
+            <img src="${producto.imagen}" >
+            <div>
             <div class="card-body " >
-                    <h5 class="card-title text-center text-dark">${producto.nombre}</h5>
+                    <p class="card-title text-center text-dark">${producto.nombre}</p>
                     
             </div>
             <div class="card-body d-flex justify-content-around align-items-center align-self-center">
                     <p  class="card-link text-dark">Precio: $${producto.precio}</p>  
-                    <button  class="card-link text-dark">Comprar</button>    
-            </div>    
-
-            
+                    <button  class="d-flex  mb-3 btn btn-secondary">Comprar</button>
+            </div>
         </div>`
         cardsContainer.appendChild(newCard)
         });
