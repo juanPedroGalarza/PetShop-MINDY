1243223a8c7bf7582193fa58e2a76440c82f44db
juanPedroGalarza <juanpg232@gmail.com>
2022-08-11 20:10:03 -0300

    codigo optimizado

--- scripts/components.js
+++ scripts/components.js
@@ -2,16 +2,10 @@ const menu = document.getElementById('burguer');
 const nav = document.getElementById('nav');
 const carrito = document.getElementById('carrito');
 const listaCarrito = document.getElementById('listaCarrito');
-
-let contador = 0
-menu.addEventListener('click', () =>{
-  contador ++
-  contador % 2 != 0 ? nav.classList.remove('hidden2') : nav.classList.add('hidden2')
+menu.addEventListener('click', () => {
+  nav.classList.toggle('hidden2')
 })
 
-
-let contador2 = 0
 carrito.addEventListener('click', () =>{
-  contador2 ++
-  contador2 % 2 != 0 ? listaCarrito.classList.remove('hidden1') : listaCarrito.classList.add('hidden1')
+  listaCarrito.classList.toggle("hidden1")
 })
