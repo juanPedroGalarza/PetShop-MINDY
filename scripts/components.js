1e7444c877eb964aebd577eba0075e50f61aa977
juanPedroGalarza <juanpg232@gmail.com>
2022-08-11 21:27:36 -0300

    carrito flotante agregado

--- scripts/components.js
+++ scripts/components.js
@@ -1,11 +1,48 @@
 const menu = document.getElementById('burguer');
 const nav = document.getElementById('nav');
-const carrito = document.getElementById('carrito');
+const carritoImg = document.querySelector('#carrito');
 const listaCarrito = document.getElementById('listaCarrito');
 menu.addEventListener('click', () => {
-  nav.classList.toggle('hidden2')
+  nav.classList.toggle( 'hidden2')
 })
-
-carrito.addEventListener('click', () =>{
+carritoImg.addEventListener('click', () => {
+  let carrito = JSON.parse(localStorage.getItem("carrito"))
+  carrito ??= [[], []]
+  let productos = carrito[0]
+  let cantidades = carrito[1]
+  let limpiarCarrito = document.createElement("span")
+  limpiarCarrito.className = "clear-car d-flex justify-content-center col-sm-10 col-lg-8"
+  listaCarrito.innerHTML = ""
+  limpiarCarrito.innerHTML = "Limpiar carrito"
+  limpiarCarrito.addEventListener("click", e => {
+    localStorage.removeItem("carrito")
+    listaCarrito.innerHTML = ""
+    listaCarrito.appendChild(limpiarCarrito)
+  })
+  listaCarrito.appendChild(limpiarCarrito)
+  productos.forEach((producto, index) => {
+    let articulo = document.createElement("div")
+    articulo.className = "carrito-element d-flex justify-content-between align-content-center col-sm-10 col-lg-8"
+    articulo.innerHTML =
+    `<img src="${producto.imagen}" alt="logo petshop" class="img-carrito">
+    <p>${producto.nombre}</p>
+    <p>$${producto.precio}</p>
+    <button type="button" class="${producto._id} btn-close" aria-label="Close"></button>`
+    articulo.addEventListener("click", e => {
+      if (e.target.classList.contains("btn-close")) {
+        productos = productos.filter(prod => prod._id != e.target.classList
+          .values().next().value)
+        cantidades = cantidades.filter((x, indx) => indx != index)
+        carrito[0] = productos
+        carrito[1] = cantidades
+        localStorage.setItem("carrito",JSON.stringify(carrito))
+        if (productos.length < 2) {
+          localStorage.removeItem("carrito")
+        }
+        e.target.parentElement.remove()
+      }
+    })
+    listaCarrito.appendChild(articulo)
+  })
   listaCarrito.classList.toggle("hidden1")
 })
